81348e006e7830e66ac1a6ef36ce398e0a7cb1e6
Matheus Fellipi <matheus.fellipi@outlook.com>
2023-12-13 19:50:55 -0300

    feat[icons]: config assests icons

--- assets/indext.ts
+++ assets/indext.ts
@@ -1,9 +1,11 @@
 import { Categories } from "./categories";
 import { Characteristics } from "./characteristics";
 import { Nav } from "./nav";
+import MarvelLogo from "./MarvelLogo.svg";
 
 export const Icons = {
   Characteristics,
   Categories,
   Nav,
+  MarvelLogo
 };
