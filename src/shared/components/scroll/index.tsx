6bbb3c0308df68e6d1de3c7e8fd03972c97c3f7d
Matheus Fellipi <matheus.fellipi@outlook.com>
2023-12-14 14:22:06 -0300

    feat[components]: add sroll componets

---
+++ src/shared/components/scroll/index.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, View } from "react-native";
+import { useTheme } from "styled-components/native";
+
+type Props = {
+  children: ReactNode;
+  bgColor?: string;
+};
+
+export function Scroll({ children, bgColor }: Props) {
+  const theme = useTheme();
+  return (
+    <SafeAreaView testID="layout" edges={["left", "right"]}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            backgroundColor: bgColor ?? theme.colors.white,
+            flex: 1,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: bgColor ?? theme.colors.white,
+              height: 400,
+              position: "absolute",
+              top: -400,
+              left: 0,
+              right: 0,
+            }}
+          />
+          {children}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
