66b534b540bd527f51e8b61d743f5d56721f2813
Matheus Fellipi <matheus.fellipi@outlook.com>
2023-12-15 12:44:59 -0300

    feat[components]: create components text

---
+++ src/shared/components/text/index.tsx
@@ -0,0 +1,33 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import { Text } from "./styled";
+import { TextComponentsProps } from "@/types/components/text";
+
+export function TextComponent({
+  fontSize = 16,
+  TextColor,
+  fontFamily,
+  margin,
+  padding,
+  uppercase,
+  textTransform = "none",
+  maxWidth,
+  children,
+  ...props
+}: TextComponentsProps) {
+  const text = {
+    fontSize: RFValue(fontSize),
+    fontFamily: fontFamily,
+    TextColor,
+    uppercase: uppercase,
+    maxWidth: maxWidth,
+    textTransform,
+    margin,
+    padding,
+  };
+
+  return (
+    <Text text={text} accessible={true} {...props}>
+      {children}
+    </Text>
+  );
+}
