43e451b44f52b33d55fc15db5698045827b556cb
Matheus Fellipi <matheus.fellipi@outlook.com>
2023-12-15 12:46:38 -0300

    feat[components]: create components input

---
+++ src/shared/components/text/styled.ts
@@ -0,0 +1,20 @@
+import { TextComponentsProps } from "@/types/components/text";
+import styled, { css } from "styled-components/native";
+
+type Props = {
+  text: TextComponentsProps;
+};
+
+export const Text = styled.Text<Props>`
+  ${({ text }) => css`
+    margin: ${text?.margin?.top ?? 0}px ${text?.margin?.right ?? 0}px
+      ${text?.margin?.bottom ?? 0}px ${text?.margin?.left ?? 0}px;
+    padding: ${text?.padding?.top ?? 0}px ${text?.padding?.right ?? 0}px
+      ${text?.padding?.bottom ?? 0}px ${text?.padding?.left ?? 0}px;
+    color: ${(props) => text?.TextColor ?? props.theme.colors.dark};
+    font-size: ${text?.fontSize}px;
+    font-family: ${text?.fontFamily ?? "Poppins_400Regular"};
+    text-transform: ${text.textTransform};
+    ${text.maxWidth && `max-width:${text.maxWidth}px;`}
+  `}
+`;
