c2047ed57c4da9def7b7915382a53e9b3f49d8cf
Matheus Fellipi <matheus.fellipi@outlook.com>
2023-12-14 22:04:41 -0300

    feat[type]: add types for details page

--- src/types/components/heros/index.tsx
+++ src/types/components/heros/index.tsx
@@ -1,6 +1,7 @@
 export type TypeCharacters = {
   id: number;
-  name: string;
+  name?: string;
+  title?: string;
   thumbnail: {
     path: string;
     extension: string;
@@ -15,4 +16,27 @@ export type CardsComponentsProps = {
 export type CardsCarrosselComponentsProps = {
   data: TypeCharacters[];
   handleRoute: (id: number) => void;
+};
+
+export type TypeCharactersDetails = {
+  id: number;
+  name: string;
+  description: string;
+
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+  comics: {
+    available: number;
+  };
+  stories: {
+    available: number;
+  };
+  events: {
+    available: number;
+  };
+  series: {
+    available: number;
+  };
 };
