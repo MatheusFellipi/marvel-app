59b206c7890ee099e9defc1fe6e30a0d53185255
Matheus Fellipi <matheus.fellipi@outlook.com>
2023-12-15 12:39:50 -0300

    refactor[assets]: refactor files svg

--- assets/index.ts
+++ assets/index.ts
@@ -1,11 +1,17 @@
-import { Categories } from "./categories";
-import { Characteristics } from "./characteristics";
-import { Nav } from "./nav";
-import MarvelLogo from "./MarvelLogo.svg";
+import { Categories } from "./svg/categories";
+import { Characteristics } from "./svg/characteristics";
+import { Nav } from "./svg/nav";
+import MarvelLogo from "./svg/MarvelLogo.svg";
+import EyeOff from "@assets/svg/icon-eye-off.svg";
+import EyeOn from "@assets/svg/icon-eye.svg";
+import Closed from "@assets/svg/icon-closed.svg";
 
 export const Icons = {
   Characteristics,
   Categories,
+  EyeOff,
+  EyeOn,
   Nav,
-  MarvelLogo
+  MarvelLogo,
+  Closed
 };
